6eaf54d5fdff15ec74684bafddfba59eb4f5385b
pablostellet <pablorstellet@gmail.com>
2017-06-20 19:17:34 -0300

    component calling other components

---
+++ ex/silvaFamily.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Member from './member'
+
+export default props => (
+    <div>
+        <Member name='Constantine' lastName='Silva' />
+        <Member name='Nandes' lastName='Silva' />
+        <Member name='Joanne' lastName='Silva' />
+        <Member name='Gabriela' lastName='Silva' />
+    </div>
+
+)
